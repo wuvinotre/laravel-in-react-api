a09f3feda2ed317c1c24abe3d24a66a92084a926
Ewerton <ewerton.viniciusturco@gmail.com>
2022-06-23 19:30:48 -0300

    Crud Products

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::controller(\App\Http\Controllers\Api\ProductController::class)->group(function(){
+    Route::get('/products', 'index');
+    Route::post('/product', 'store');
+    Route::get('/product/{id}', 'show');
+    Route::put('/product/{id}', 'update');
+    Route::delete('/product/{id}', 'destroy');
+});
+
